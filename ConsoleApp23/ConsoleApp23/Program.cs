cf4e6217f48da554bb97afa5dd1333d6cd428a6b
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2020-11-15 11:15:06 +0100

    OK

--- ConsoleApp23/ConsoleApp23/Program.cs
+++ ConsoleApp23/ConsoleApp23/Program.cs
@@ -4,9 +4,47 @@ namespace ConsoleApp23
 {
     class Program
     {
+   
         static void Main(string[] args)
         {
-            Console.WriteLine("gege");
+            PolePowierzchniObjectoscKuli();
+            string tekst = "supersuper super ";
+            var ilosc =FunkcjaPrzyjmijTekst(tekst);
+            Console.WriteLine("Ile razy Super : {0} ",ilosc);
+           
+
+
+        }
+
+        private static int FunkcjaPrzyjmijTekst(string tekst)
+        {
+            var ilosc = 0;
+            if (System.Text.RegularExpressions.Regex.IsMatch(tekst,"super"))
+            {
+                ilosc++;
+            }
+
+            return ilosc;
+        }
+
+
+        static void PolePowierzchniObjectoscKuli()
+        {
+            var r = Console.ReadLine();
+            bool udalosie = int.TryParse(r, out int rP);
+            if (udalosie)
+            {
+                Console.WriteLine("Udalo sie sparsowac");
+            }
+            else
+            {
+                Console.WriteLine("Nie dualo sie sparsowac");
+                
+            }
+            double P = 4*Math.PI * rP * rP;
+            double O = 4 / 3.0 * Math.PI * rP * rP;
+            Console.WriteLine("Pole powierzchni: {0} Objętość kuli: {1}",P,O);
+
         }
     }
 }
---
+++ ConsoleApp23/ConsoleApp23/Restauracja.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp23
+{
+    public class Restauracja
+    { 
+        private bool[] Stoliki = new bool[10];
+    private string[] opinie = new string[3];
+    private int[] oceny = new int[10];
+
+    public void zareStolik(bool stolik)
+    {
+    stolik = true;
+    }
+
+    public void opuscStolik(int oceny, int opinie)
+    {
+    }
+}
+
+}
